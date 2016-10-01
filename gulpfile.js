08c32ed589acccbffc6c744b73d036c7c246dd3c
Arun Nayak <arun@Aruns-MacBook-Air.local>
2016-10-02 00:02:34 +1000

    minification for UI pages

--- gulpfile.js
+++ gulpfile.js
@@ -7,14 +7,10 @@ var gulp = require('gulp'),
 	browserSync = require('browser-sync'),
 	concatJS = require('gulp-concat'),
 	concatCss = require('gulp-concat-css'),
-	fileinclude = require('gulp-file-include');
-
+	fileinclude = require('gulp-file-include'),
+	htmlmin = require('gulp-htmlmin');
 
 var reload = browserSync.reload;
-var SRC = './scripts/*.js';
-var DEST = 'dist/scripts';
-
-
 
 gulp.task('serve', function (){
 	browserSync.init({
@@ -25,7 +21,13 @@ gulp.task('serve', function (){
 	gulp.watch('./scripts/*.js', ['concatJS']);
 	gulp.watch('./images/*', ['compress-images']);
 	gulp.watch('./views/*.html', ['fileinclude']);
-	gulp.watch(['./views/*.html', './css/*.css', './scripts/*.js', 'images/*/**']).on('change', reload);
+	gulp.watch(['index.html', './views/*.html', './css/*.css', './scripts/*.js', 'images/*/**']).on('change', reload);
+});
+
+gulp.task('minify', function() {
+  	gulp.src('dist/*.html')
+    .pipe(htmlmin({collapseWhitespace: true}))
+    .pipe(gulp.dest('dist'));
 });
 
 gulp.task('fileinclude', function() {
@@ -35,18 +37,14 @@ gulp.task('fileinclude', function() {
       basepath: '@file'
     }))
     .pipe(gulp.dest('dist/'));
+
 });
 
 gulp.task('concatCss', function () {
    	gulp.src('./css/*.css')
     .pipe(concatCss('bundle.css'))
+    .pipe(minifyCSS('bundle.css'))
     .pipe(gulp.dest('dist/css'));
-});
-
-gulp.task('minify-css', function(){
-	gulp.src('/dist/css/bundle.css')
-	.pipe(minifyCSS('bundle.min.css'))
-	.pipe(gulp.dest('dist/css/'))
 });
 
 gulp.task('compress-images', function(){
@@ -54,34 +52,18 @@ gulp.task('compress-images', function(){
 		.pipe(imagemin({progressive: true, optimizationlevel: 7}))
 		.pipe(gulp.dest('dist/images/'));
 });
+
 gulp.task('concatJS', function (){
 	 gulp.src(['./scripts/jquery-1.11.1.min.js', './scripts/jquery.themepunch.tools.min.js', './scripts/jquery.themepunch.revolution.min.js', './scripts/jquery.cubeportfolio.min.js', './scripts/jquery.mmenu.min.js', './scripts/jquery.sticky.js', './scripts/bootstrap.min.js', './scripts/moderniz.js', './scripts/owl.carousel.js', './scripts/app.js'])
 	.pipe(concatJS('bundle.js'))
+	.pipe(uglify('bundle.js'))
 	.pipe(gulp.dest('dist/scripts'));
-});
-
-gulp.task('compressJS', function (){
-	return gulp.src('./scripts/*')
-	.pipe(uglify())
-	.pipe(gulp.dest('dist/scripts'));
-});
-
-gulp.task('changed', function(){
-	return gulp.src(SRC)
-	.pipe(changed(DEST))
-	.pipe(gulp.dest(DEST));
 });
 
 gulp.task('jshint', function (){
 	gulp.src('./scripts/*.js')
 	.pipe(jshint())
 	.pipe(jshint.reporter('default'));
-});
-
-
-
-gulp.task('watch', function(){
-	gulp.watch(SRC, ['concatCss', 'concatJS', 'compress-images', 'fileinclude']);
 });
 
 gulp.task('default', ['serve']);
